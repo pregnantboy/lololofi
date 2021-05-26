9cfe36d5b887ea0c6f451d292552fcd313d35b78
Ian Chen <ian-@gmx.com>
2021-05-26 21:41:42 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styles from './App.module.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.app}>
+      <header className={styles.center}>
+        <div className={styles.pokeball}>
+          <div className={styles.pokeballButton}></div>
+        </div>
       </header>
+      <p className={styles.text}>I own this haha</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
