7f7a961d8b60b59fa9d559afe99ff1f76ddd5a0b
Ian Chen <ian-@gmx.com>
2021-05-26 21:48:02 +0800

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
           <div className={styles.pokeballButton}></div>
         </div>
       </header>
-      <p className={styles.text}>I own this haha</p>
+      <p className={styles.text}>this is mine</p>
     </div>
   )
 }
